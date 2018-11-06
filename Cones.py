b1de515a31ac9a5257962c4dd140e658502856d7
Steven Schaefer <stschaef@umich.edu>
2018-11-06 16:51:44 -0500

    can draw tiling

--- Cones.py
+++ Cones.py
@@ -1,24 +1,16 @@
 import PIL.ImageDraw as ImageDraw
 import PIL.Image as Image
 import numpy as np
-
-# image = Image.new("RGB", (640, 480))
-#
-# draw = ImageDraw.Draw(image)
-#
-# # points = ((1,1), (2,1), (2,2), (1,2), (0.5,1.5))
-# points = ((100, 100), (200, 100), (200, 200), (100, 200), (50, 150))
-# draw.polygon((points), fill=200)
-#
-# image.show()
+import random
 
 WIDTH = 800
 HEIGHT = 600
 
 
 image = Image.new("RGB", (WIDTH, HEIGHT))
-
+image_2 = Image.new("RGB", (WIDTH, HEIGHT))
 draw = ImageDraw.Draw(image)
+draw_2 = ImageDraw.Draw(image_2)
 
 B= np.array([
     [2, -1, -3],
@@ -72,9 +64,8 @@ def drawable(pt):
     return x + offset[0], y + offset[1]
 
 
-
 def tiling(matrices):
-    print("Line")
+
     for mat in matrices:
         p1 = proj(np.dot(mat, L1))
         p2 = proj(np.dot(mat, L2))
@@ -86,10 +77,12 @@ def tiling(matrices):
         p3 = drawable(p3)
         p4 = drawable(p4)
 
-        draw.line((p1, p2, p3, p4), fill=200, width=2)
-
+        # color = (random.randrange(125, 255), random.randrange(125, 155), random.randrange(155, 255))
+        draw.line((p1, p2, p3, p4), fill=(255, 255, 255), width=2)
+        # draw.polygon((p1, p2, p3), fill=color)
 
 def colored_tiling(matrices):
+    # Don't know how to incorporate this function
     for mat in matrices:
         p1 = proj(np.dot(mat, L1))
         p2 = proj(np.dot(mat, L2))
@@ -102,46 +95,30 @@ def colored_tiling(matrices):
         # print(p1, p2, p3)
         draw.polygon((p1, p2, p3), fill=200)
 
-# draw_these_lines=[matrix.identity(3),R1*R2,R1*R2*R1*R2,R1,R1*R2*R1,R1*R2*R1*R2*R1,R3]
-# draw_these_polygons=[matrix.identity(3),R1*R2,R1*R2*R1*R2]
-# draw_these3=[(R1*R2*R3)^i for i in [-4..4]]
-#     tiling(draw_these3)+
-#     tiling(draw_these_lines)+
-#      colored_tiling(draw_these_polygons)
-# ).show(axes=false,aspect_ratio=1)
-
 
 mats = [Id, R1, R2, R3]
 
-COUNT_MAX = 500
+COUNT_MAX = 1500
 
 def gen_mats(matrices):
     count = 0
     for mat in mats:
-        if not any((np.linalg.inv(mat) == x).all() for x in mats):
-            mats.append(np.linalg.inv(mat))
-            count += 1
-            print('that')
-            if count > COUNT_MAX:
-                return
+        # Redundant check, they are reflections so they are their own inverses
+        # if not any((np.linalg.inv(mat) == x).all() for x in mats):
+        #     mats.append(np.linalg.inv(mat))
+        #     count += 1
+        #     if count > COUNT_MAX:
+        #         return
         for matrix in mats:
             if not any((np.dot(mat, matrix) == x).all() for x in mats):
                 mats.append(np.dot(mat, matrix))
                 count += 1
-                print('this')
                 if count > COUNT_MAX:
                     return
 
 
 gen_mats(mats)
 print(mats)
-
-# draw_these_lines=[Id,R1*R2,R1*R2*R1*R2,R1,R1*R2*R1,R1*R2*R1*R2*R1,R3]
-# mats = [Id, R1*R2,R1*R2*R1*R2,R1,R1*R2*R1,R1*R2*R1*R2*R1]
 tiling(mats)
-# tiling(draw_these_lines)
-#colored_tiling(mats)
-# points = ((100, 100), (200, 100), (200, 200), (100, 200), (50, 150))
-# draw.polygon((points), fill=200)
 image.show()
 
